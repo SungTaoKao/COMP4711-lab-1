b8eedd21626fa15ddb4456b86d6dc7253e58511a
pkao <paul_kao@hotmail.ca>
2017-09-10 14:31:43 -0700

    added pictures to each profile

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 	<title>COMP 4711 - LAB 1</title>
 </head>
 <body>
+	<h1><b>COMP4711 : LAB 1</b></h1>
 	<p>
 		<?php
 			include('Student.php');
-
+			
+			//create an empty student array
 			$students = array();
 			
+			//dummy data - Student 1
 			$first = new Student();
 			$first->surname = "Doe";
 			$first->first_name = "John";
@@ -17,8 +20,12 @@
 			$first->add_grade(65);
 			$first->add_grade(75);
 			$first->add_grade(55);
+			
+			//uses catapi to generate random cat picture
+			$first->change_picture("http://thecatapi.com/api/images/get?format=src&type=gif");
 			$students['j123'] = $first;
 			
+			//dummy data - Student 2
 			$second = new Student();
 			$second->surname = "Einstein";
 			$second->first_name = "Albert";
@@ -29,7 +36,22 @@
 			$second->add_grade(80);
 			$second->add_grade(50);
 			$students['a456'] = $second;
-
+			
+			//dummy data - Student 3
+			$studentMe = new Student();
+			$studentMe->surname = "Kao";
+			$studentMe->first_name = "Paul";
+			$studentMe->add_email('home', 'home@email.com');
+			$studentMe->add_email('work', 'work@email.com');
+			$studentMe->add_grade(50);
+			$studentMe->add_grade(75);
+			$studentMe->add_grade(85);
+			$students['a000'] = $studentMe;
+			
+			//sort array by key
+			ksort($students);
+			
+			//output each student in the array
 			foreach($students as $student)
 				echo $student->toString();
 		?>
--- student.php
+++ student.php
@@ -1,22 +1,43 @@
 <?php
 
 class Student {
-	
+	/*
+		student constructor
+		$surname - string; student last name
+		$firstname - string; student first name
+		$emails - array; list of student's emails
+		$grades - array; list of student's grades
+		$picture - string; default picture url
+	*/
 	function __construct() {
 		$this->surname = '';
 		$this->first_name = '';
 		$this->emails = array();
 		$this->grades = array();
+		$this->picture = "http://pasp.org.pk/assets/img/pic.png";
 	}
 	
+	/*
+		add email function
+		$which - string; specifies index of array to put the email
+		$address - string; email to be added
+	*/
 	function add_email($which, $address) {
 		$this->emails[$which] = $address;
 	}
 	
+	/*
+		add grade function
+		$grade - int; grade to be added to next available index
+	*/
 	function add_grade($grade) {
 		$this->grades[] = $grade;
+		
 	}
 	
+	/*
+		average calculating function
+	*/
 	function average() {
 		$total = 0;
 		foreach ($this->grades as $value)
@@ -24,13 +45,23 @@ class Student {
 		return $total / count($this->grades);
 	}
 	
+	/*
+		change picture function
+	*/
+	function change_picture($newURL){
+			$this->picture = $newURL;
+	}
+	
+	/*
+		output String function into rendered html
+	*/
 	function toString() {
 		$result = $this->first_name . ' ' . $this->surname;
 		$result .= ' ('.$this->average().")\n";
 		foreach($this->emails as $which=>$what)
 			$result .= $which . ': '. $what. "\n";
 		$result .= "\n";
-		return '<pre>'.$result.'</pre>';
+		return '<pre><div style="border: 2px solid black; padding: 5px; align: center"><img src="'.$this->picture.'" alt="student"/><br><br><br>'.$result.'</div></pre>';
 	}
 }
 
